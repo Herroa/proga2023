1fb42eb8a05acd72d8d5436316a0a4b5fefaf17f
Herroa <roman.gordov@mail.ru>
2023-02-06 16:59:13 +0700

    origin

--- 1.c
+++ 1.c
@@ -1,6 +1,24 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
+void init(int* arr, int n)
+{
+    arr = malloc(n * sizeof(int));
+    int i;
+    for (i = 0; i < n; ++i)
+    {
+        arr[i] = i;
+    }
+}
+int main()
+{
+    int* arr = NULL;
+    int n = 10;
 
-int main(){
-    printf("%d\n",1);
+    init(arr, n);
+    int i;
+    for (i = 0; i < n; ++i)
+    {
+        printf("%d\n", arr[i]);
+    }
     return 0;
 }
