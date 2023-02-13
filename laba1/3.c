f5909e06d704fba28ce3c66180d0635a5ee6b1fd
Herroa <roman.gordov@mail.ru>
2023-02-13 14:10:47 +0700

    sqr change

--- laba1/3.c
+++ laba1/3.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-#define SQR(x) x * x
+#define SQR(x) ((x) * (x))
 
 int main()
 {
-    int y = 5;
-    int z = SQR((y + 1));
+    int y = 1;
+    double z = 1.0 / SQR(y + 1);
     //dobavlyaem (), y + 1 => (y + 1)
-    printf("z = %d\n", z);
+    printf("z = %lf\n", z);
     return 0;
 }
