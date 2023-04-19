4c507ac0ecac0e63f7f94c5f39f97a081c30217f
Herroa <roman.gordov@mail.ru>
2023-04-19 18:17:00 +0700

    encode

---
+++ laba3_bit_ops/src/coder.c
@@ -0,0 +1,44 @@
+#include "coder.h"
+
+int encode(uint32_t code_point, CodeUnit *code_unit)
+{
+    if(code_point < 0x80){
+        // 1000 0000
+        code_unit -> length = 1;
+        code_unit -> code[0] = code_point;
+    }
+    else if(code_point < 0x800){
+        // 1000 0000 0000
+        code_unit -> length = 2;
+        code_unit -> code[0] = 0xc0 | (code_point >> 6);
+        // 1100 0000
+        code_unit -> code[1] = 0x80 | (code_point & 0x3f); 
+    }
+    else if(code_point < 0x1000){
+        // 1 0000 0000 0000 0000
+        code_unit -> length = 3;
+        code_unit -> code[0] = 0xe0 | (code_point >> 12);
+        // 1110 0000
+        code_unit -> code[1] = 0x80 | ((code_point >> 6) & 0x3f);
+        code_unit -> code[2] = 0x80 | (code_point & 0x3f);
+    }
+    else if(code_point < 0x200000){
+        // 10 0000 0000 0000 0000
+        code_unit -> length = 4;
+        code_unit -> code[0] = 0xf0 | (code_point >> 18);
+        // 1111 0000
+        code_unit -> code[1] = 0x80 | ((code_point >> 12) & 0x3f) ;
+        code_unit -> code[2] = 0x80 | ((code_point >> 6) & 0x3f);
+        code_unit -> code[3] = 0x80 | (code_point & 0x3f);
+    }
+    else{
+        return -1;
+    }
+    return 0;
+}
+
+int write_code_unit(FILE *out, const CodeUnit *code_unit)
+{
+    int result = fwrite(code_unit->code, 1, code_unit->length, out);
+    return result;
+}
