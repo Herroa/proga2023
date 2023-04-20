80951509eb6df0f4f25436694545a9f668167c1e
Herroa <roman.gordov@mail.ru>
2023-04-20 13:51:58 +0700

    read unit

--- laba3_bit_ops/src/coder.c
+++ laba3_bit_ops/src/coder.c
@@ -14,7 +14,7 @@ int encode(uint32_t code_point, CodeUnit *code_unit)
         // 1100 0000
         code_unit -> code[1] = 0x80 | (code_point & 0x3f); 
     }
-    else if(code_point < 0x1000){
+    else if(code_point < 0x10000){
         // 1 0000 0000 0000 0000
         code_unit -> length = 3;
         code_unit -> code[0] = 0xe0 | (code_point >> 12);
@@ -41,4 +41,81 @@ int write_code_unit(FILE *out, const CodeUnit *code_unit)
 {
     int result = fwrite(code_unit->code, 1, code_unit->length, out);
     return result;
+}
+
+int read_next_code_unit(FILE *in, CodeUnit *code_unit)
+{
+    uint8_t byte;
+    fread(&byte, sizeof(uint8_t), 1, in);
+
+    if(byte<0x80){
+        code_unit -> length = 1;
+        code_unit -> code[0] = byte;
+        return 0;
+    }
+    else if(byte < 0xe0){
+        //111xxxxx
+        code_unit -> length = 2;
+        code_unit -> code[0] = byte;
+        fread(&byte, sizeof(uint8_t), 1, in);
+        if(byte < 0xc0){
+            //11xxxxxx
+            code_unit -> code[1] = byte;
+        }
+        else{
+            printf("bitiy");
+        }
+        return 0;
+    }
+    else if(byte < 0xf0){
+        //1111xxxx
+        code_unit -> length = 3;
+        code_unit -> code[0] = byte;
+        fread(&byte, sizeof(uint8_t), 1, in);
+        if(byte < 0xc0){
+            code_unit -> code[1] = byte;
+            fread(&byte, sizeof(uint8_t), 1, in);
+            if(byte < 0xc0){
+                code_unit -> code[2] = byte;
+            }
+            else{
+                printf("bitiy");
+            }
+        }
+        else{
+            printf("bitiy");
+        }
+        return 0;
+    }
+    else if(byte < 0xf8){
+        //11111xxx
+        code_unit -> length = 4;
+        code_unit -> code[0] = byte;
+        fread(&byte, sizeof(uint8_t), 1, in);
+        if(byte < 0xc0){
+            code_unit -> code[1] = byte;
+            fread(&byte, sizeof(uint8_t), 1, in);
+            if(byte < 0xc0){
+                code_unit -> code[2] = byte;
+                fread(&byte, sizeof(uint8_t), 1, in);
+                if(byte < 0xc0){
+                    code_unit -> code[3] = byte;
+                }
+                else{
+                    printf("bitiy");
+                }
+            }
+            else{
+                printf("bitiy");
+            }
+        }
+        else{
+            printf("bitiy");
+        }
+        return 0;
+    }
+    else{
+        read_next_code_unit(in, code_unit);
+        return -1;
+    }
 }
