3d467838dc3f9f7d7029bf83dee2fbfafbfddf3f
Herroa <roman.gordov@mail.ru>
2023-05-22 19:58:57 +0700

    ready?

--- coursework/src/dir.c
+++ coursework/src/dir.c
@@ -9,10 +9,10 @@ int nextdir(char *template, char *path)
 {
     DIR *dir;
     struct dirent *dp;
-    char *temp=malloc(sizeof(char)*255);
+    char *temp = malloc(sizeof(char) * 255);
     int flag;
-    flag = thisdir(template,path);
-    if(flag==1)
+    flag = thisdir(template, path);
+    if (flag == 1)
     {
         return 1;
     }
@@ -22,13 +22,14 @@ int nextdir(char *template, char *path)
         printf("Cant open directory!\n");
         return 1;
     }
-    while ((dp = readdir(dir)) != NULL){
+    while ((dp = readdir(dir)) != NULL)
+    {
         if (dp->d_type == DT_DIR && strcmp(dp->d_name, "..") != 0 && strcmp(dp->d_name, ".") != 0)
         {
             int length = strlen(dp->d_name);
             char cur_dir[length];
-            
-            strcpy(cur_dir,dp->d_name);
+
+            strcpy(cur_dir, dp->d_name);
             strcpy(temp, path);
             strcat(temp, "/");
             strcat(temp, cur_dir);
@@ -51,23 +52,23 @@ int thisdir(char *template, char *path)
         return 1;
     }
     int len = strlen(path);
-    char temp[len+1];
-    strcpy(temp,path);
+    char temp[len + 1];
+    strcpy(temp, path);
 
     printf("Open dir '%s'\n", path);
-    while ((dp = readdir(dir)) != NULL){
-        if (dp->d_type != 4)
-        {   
+    while ((dp = readdir(dir)) != 0)
+    {
+        if (dp->d_type != 4 && strcmp(dp->d_name,"bmatcher")!=0)
+        {
             path = malloc(255 * sizeof(char));
             strcpy(path, temp);
             strcat(path, "/");
             strcat(path, dp->d_name);
-            check_file(template,path);
+            check_file(template, path);
             free(path);
         }
     }
     closedir(dir);
-    printf("^^^\n");
     return 0;
 }
 
@@ -77,7 +78,7 @@ int check_file(char *template, const char *path)
     FILE *in = fopen(path, "r");
     if (!in)
     {
-        printf("Can't open file: %s!\n",path);
+        printf("Can't open file: %s!\n", path);
         return 1;
     }
     printf("In file '%s':\n", path);
